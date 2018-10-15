947d625cc1ba07e1ab7dc4351b38a9fe90c2ce6f
DevAbas <abas.memmedov.94@gmail.com>
2018-10-15 23:11:02 +0400

    Validation made for Movie Schema

--- models/Movie.js
+++ models/Movie.js
@@ -5,11 +5,25 @@ const MovieSchema = new Schema({
   director_id: Schema.Types.ObjectId,
   title: {
     type: String,
-    required: true
+    required: [true, '`{PATH}` is required!'],
+    maxlength: [15, '`{PATH}` must be smaller than `{MAXLENGTH}`!'],
+    minlength: [3, '`{PATH}` must be bigger than `{MINLENGTH}`!'], 
   },
-  category: String,
-  country: String,
-  year: Number,
+  category: {
+    type: String,
+    maxlength: 30,
+    minlength: 1
+  },
+  country: {
+    type: String,
+    maxlength: 30,
+    minlength: 1
+  },
+  year: {
+    type: Number,
+    max: 2040,
+    min: 1900
+  },
   imdb_score: Number,
   createdAt: {
     type: Date,
